a086975e937403c6599de34fcdb6d1d5ca5faaf6
Liumingyao666 <1806859182@qq.com>
2023-07-30 16:04:49 +0800

    Feign

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,11 +1,11 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -14,16 +14,26 @@ public class OrderService {
     private OrderMapper orderMapper;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
-        // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // 2.利用RestTemplate发送Http请求，查询用户
-        String url = "http://userservice/user/" + order.getUserId();
-        //发送http请求，实现远程调用
-        User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.findById(order.getUserId());
         order.setUser(user);
         return order;
     }
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//        // 2.利用RestTemplate发送Http请求，查询用户
+//        String url = "http://userservice/user/" + order.getUserId();
+//        //发送http请求，实现远程调用
+//        User user = restTemplate.getForObject(url, User.class);
+//        order.setUser(user);
+//        return order;
+//    }
 }
