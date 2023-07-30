77fe1d068ff4b1fe56e9d3abc5c48f7ce0ab7ae7
Liumingyao666 <1806859182@qq.com>
2023-07-30 21:12:37 +0800

    自定义全局过滤器

---
+++ gateway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
@@ -0,0 +1,48 @@
+package cn.itcast.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 自定义全局过滤器
+ *需求：定义全局过滤器，拦截请求，判断请求的参数是否满足下面条件：
+ *
+ * - 参数中是否有authorization，
+ *
+ * - authorization参数值是否为admin
+ *
+ * 如果同时满足则放行，否则拦截
+ */
+//@Order(-1)
+@Component
+public class AuthorizeFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1.获取请求参数
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String, String> params = request.getQueryParams();
+        // 2.获取参数中的authorize参数
+        String auth = params.getFirst("authorization");
+        // 3.判断参数值是否为admin
+        if ("admin".equals(auth)) {
+            // 4.是，放行
+            return chain.filter(exchange);
+        }
+        // 5.否，拦截
+        // 5.1 设置状态码
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
